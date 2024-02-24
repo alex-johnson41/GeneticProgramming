cac4ce90da7728fdb5430d3848623cecdd648ac0
alex-johnson41 <alex@heathjohnson.org>
2024-02-23 18:38:44 -0600

    Add deletion mutation

--- GeneticProgramming/mutator/JoyMutator.cs
+++ GeneticProgramming/mutator/JoyMutator.cs
@@ -25,12 +25,16 @@ namespace GeneticProgramming
             while (newPopulation.Count < populationSize)
             {
                 IGenome newGenome;
-                if (Random.NextDouble() < 0.5) // TODO: Make this a parameter that gets passed in (crossoverRate)
+                double rand = Random.NextDouble();
+                if (rand < 0.5) // TODO: Make this a parameter that gets passed in (crossoverRate)
                     newGenome = CrossoverGenome(population[Random.Next(population.Count)], population[Random.Next(population.Count)]);
-                else{
+                else if (rand < 0.9) // TODO: Make this a parameter that gets passed in (crossoverRate + mutationRate)
                     newGenome = MutateGenome(population[Random.Next(population.Count)]);
-                }
-                if (!JoyUtils.SyntaxErrors(newGenome.Program) && newGenome.Program.Count <= maxProgramLength)
+                else 
+                    newGenome = DeleteGenome(population[Random.Next(population.Count)]);
+                if (newGenome.Program.Count <= maxProgramLength
+                    && newGenome.Program.Count > 0
+                    && !JoyUtils.SyntaxErrors(newGenome.Program))
                     newPopulation.Add(newGenome);
             }
             return newPopulation;
@@ -51,6 +55,14 @@ namespace GeneticProgramming
             int addIndex = Random.Next(g2.Program.Count);
             newProgram.RemoveRange(removeIndex, newProgram.Count - removeIndex);
             newProgram.AddRange(g2.Program.GetRange(addIndex, g2.Program.Count - addIndex));
+            return new JoyGenome(newProgram);
+        }
+
+        private JoyGenome DeleteGenome(IGenome genome)
+        {
+            List<string> newProgram = new List<string>(genome.Program);
+            int index = Random.Next(newProgram.Count);
+            newProgram.RemoveAt(index);
             return new JoyGenome(newProgram);
         }
     }
