5e1936d9e9b086d9c40203a21dd1911296f8c484
alex-johnson41 <alex@heathjohnson.org>
2024-04-24 12:33:17 -0500

    Update main method

--- GeneticProgramming/MainClass.cs
+++ GeneticProgramming/MainClass.cs
@@ -1,23 +1,29 @@
-namespace GeneticProgramming
+﻿namespace GeneticProgramming
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
             int populationSize = 5000;
-            int maxProgramLength = 150;
-            int numSimulators = 5; 
+            int maxProgramLength = 75;
+            int numSimulators = 10; 
 
             List<Thread> threads = new List<Thread>();
 
             for (int i = 0; i < numSimulators; i++)
             {
-                Simulator sim = new Simulator(populationSize,
-                                maxProgramLength,
-                                new JoyFitness(fuelCostTestCases),
-                                new JoyMutator(new Random()),
-                                i
-                                );
+                Simulator sim = new Simulator(
+                    populationSize,
+                    maxProgramLength,
+                    new JoyFitness(fibonacciNumbers),
+                    new JoyMutator(
+                        0.5f,  // Survival percentage
+                        0.7f,  // Crossover percentage
+                        0.25f, // Mutation percentage
+                        0.05f  // Deletion percentage
+                        ),     // The last three floats must add to 1
+                    i
+                    );
                 Thread thread = new Thread(() => sim.Run(null));
                 threads.Add(thread);
                 thread.Start();
@@ -43,81 +49,56 @@ namespace GeneticProgramming
 
         private static Dictionary<List<int>, float> fuelCostTestCases = new Dictionary<List<int>, float>()
         {
-            { new List<int> {9, 12, 15, 18, 21}, 15},
-            { new List<int> {6, 9, 12, 15, 18}, 10},
-            { new List<int> {3, 6, 9, 12, 15}, 5},
-            { new List<int> {10, 20, 30, 40, 50}, 38},
-            { new List<int> {1, 4, 7, 10, 13}, 0},
-            { new List<int> {8, 11, 14, 17, 20}, 10},
-            { new List<int> {5, 8, 11, 14, 17}, 5},
-            { new List<int> {2, 5, 8, 11, 14}, 0},
-            { new List<int> {15, 25, 35, 45, 55}, 47},
-            { new List<int> {18, 21, 24, 27, 30}, 30},
-            { new List<int> {3, 5, 19}, 2},
-            { new List<int> {57, 34, 2}, 24},
-            { new List<int> {7, 1, 46}, 11},
-            { new List<int> {1}, -2},
-            { new List<int> {2}, -2},
-            { new List<int> {3}, -1},
-            { new List<int> {4}, -1},
-            { new List<int> {5}, -1},
-            { new List<int> {6}, 0},
-            { new List<int> {7}, 0},
-            { new List<int> {8}, 0},
-            { new List<int> {9}, 1},
-            { new List<int> {10}, 1},
-            { new List<int> {11}, 1},
-            { new List<int> {12}, 2},
-            { new List<int> {30, 15, 66}, 31 },
-            { new List<int> {70, 74, 42, 3, 2, 71, 59, 8, 62, 11}, 109 },
-            { new List<int> {30, 75, 49, 30, 63, 65, 33, 90, 62}, 146 },
-            { new List<int> {9, 76, 20, 94, 37, 83, 63, 50}, 125 },
-            { new List<int> {93, 40, 22, 22}, 50 },
-            { new List<int> {94, 31, 86, 70, 73}, 106 },
-            { new List<int> {47, 78, 16, 84, 79, 45}, 103 },
-            { new List<int> {16, 95, 97, 78, 49, 96, 2}, 128 },
-            { new List<int> {16, 75, 70, 76, 77, 10}, 94 },
-            { new List<int> {100, 35, 69, 75, 66, 10, 4, 5}, 103 },
-            { new List<int> {89, 70, 88, 66, 73, 72, 28, 67, 65, 38}, 195 },
-            { new List<int> {10, 27, 78}, 32 },
-            { new List<int> {44, 95, 29, 68, 36, 64}, 97 },
-            { new List<int> {54, 54, 51, 11, 31, 16, 3}, 58 },
-            { new List<int> {17, 44, 80, 84, 6, 100, 66, 96, 89}, 173 },
-            { new List<int> {8, 38, 54, 48, 99, 49, 53}, 100 },
-            { new List<int> {9, 77, 8, 98}, 54 },
-            { new List<int> {99, 74, 13, 61, 56, 75, 31, 91, 59, 47}, 178 },
-            { new List<int> {37, 44, 66, 25, 42, 26, 73, 21, 25, 53}, 114 },
-            { new List<int> {70, 25, 66, 35, 74, 72, 89}, 127 },
-            { new List<int> {55, 93, 80, 26, 100, 90, 88, 79}, 185 },
-            { new List<int> {19, 70, 39, 77, 68, 14, 16, 84, 54, 36}, 136 },
-            { new List<int> {93, 31, 27, 4, 18, 37, 85, 64, 48, 3}, 115 },
-            { new List<int> {20, 99, 37, 5, 3, 67, 5}, 62 },
-            { new List<int> {41, 82, 12, 55, 40, 99, 30}, 104 },
-            { new List<int> {42, 35, 33, 22, 57}, 52 },
-            { new List<int> {13, 11, 24, 27, 28, 68, 82}, 68 },
-            { new List<int> {13, 11, 23, 41, 35, 83, 69, 48, 49}, 102 },
-            { new List<int> {12, 48, 4}, 15 },
-            { new List<int> {55, 56, 9, 77, 30, 55, 50}, 94 },
-            { new List<int> {70, 69, 49, 20, 84, 87, 32, 15, 58}, 141 },
-            { new List<int> {53, 33, 90, 87, 35, 53, 46, 47, 53}, 144 },
-            { new List<int> {43, 77, 73, 64, 43, 36, 53, 5}, 112 },
-            { new List<int> {80, 49, 96, 96, 84}, 124 },
-            { new List<int> {73, 59, 32, 62, 54, 87, 20, 30, 12}, 122 },
-            { new List<int> {83, 56, 68, 41, 2, 39}, 81 },
-            { new List<int> {86, 51, 38, 64, 92, 76, 79}, 145 },
-            { new List<int> {76, 61, 95, 39, 21, 52, 12, 34}, 112 },
-            { new List<int> {64, 53, 9, 1, 20, 64, 78}, 80 },
-            { new List<int> {75, 37, 12, 81, 49, 5}, 73 },
-            { new List<int> {46, 71, 78, 4, 11, 13, 99, 82, 39}, 127 },
-            { new List<int> {71, 21, 28, 22}, 38 },
-            { new List<int> {82, 40, 94, 37, 65, 48, 28, 35, 68, 31}, 152 },
-            { new List<int> {55, 10, 61, 26, 61, 57}, 76 },
-            { new List<int> {18, 73, 20, 45, 1, 90, 21, 92, 97, 55}, 148 },
-            { new List<int> {72, 53, 40}, 48 },
-            { new List<int> {56, 15, 64, 68, 31, 3, 26, 67, 20}, 95 },
-            { new List<int> {42, 10, 36, 94}, 52 },
-            { new List<int> {9, 48, 72, 52, 6}, 52 },
-            { new List<int> {21, 62, 32, 36, 20, 1, 63, 39, 63}, 92 },
+            { new List<int> {77, 68, 31, 34, 62, 48, 49}, 106f },
+            { new List<int> {83}, 25f },
+            { new List<int> {19, 50}, 18f },
+            { new List<int> {6, 56, 38, 85, 44, 87}, 91f },
+            { new List<int> {46, 52, 36}, 38f },
+            { new List<int> {17, 100, 90, 37}, 72f },
+            { new List<int> {99, 6, 39, 65}, 61f },
+            { new List<int> {82, 70, 45, 15}, 62f },
+            { new List<int> {12, 84, 23, 32, 100, 93}, 101f },
+            { new List<int> {93, 32, 15, 91, 99, 88, 32}, 134f },
+            { new List<int> {35, 44, 22, 86, 16, 51}, 70f },
+            { new List<int> {62}, 18f },
+            { new List<int> {44}, 12f },
+            { new List<int> {39}, 11f },
+            { new List<int> {11}, 1f },
+            { new List<int> {58, 10, 23, 54}, 39f },
+            { new List<int> {71, 62, 19}, 43f },
+            { new List<int> {89}, 27f },
+            { new List<int> {62, 11, 35, 66, 97, 36, 10}, 89f },
+            { new List<int> {74, 40, 91, 18, 87}, 92f },
+            { new List<int> {76, 63, 41, 20}, 57f },
+            { new List<int> {63}, 19f },
+            { new List<int> {36, 38, 8, 31, 64, 44}, 59f },
+            { new List<int> {64, 35, 92, 6, 56, 37, 10}, 83f },
+            { new List<int> {41, 98, 65, 68, 81, 40, 65}, 135f },
+            { new List<int> {90, 66, 60, 49, 9, 65}, 100f },
+            { new List<int> {27, 39, 47, 31, 95}, 68f },
+            { new List<int> {12, 8, 99, 44}, 45f },
+            { new List<int> {94, 66}, 49f },
+            { new List<int> {60, 47, 43, 20, 41, 39, 13}, 71f },
+            { new List<int> {26, 88, 48, 20, 53}, 66f },
+            { new List<int> {56, 9, 10, 23}, 23f },
+            { new List<int> {46, 65, 37, 6, 59, 7}, 59f },
+            { new List<int> {33, 40, 89, 41, 45}, 71f },
+            { new List<int> {99, 20}, 35f },
+            { new List<int> {76, 25, 59, 49, 54, 35}, 85f },
+            { new List<int> {76, 94, 62}, 70f },
+            { new List<int> {40, 73, 36, 69, 91, 49, 27}, 113f },
+            { new List<int> {89, 6}, 27f },
+            { new List<int> {48, 88, 93, 70, 84}, 117f },
+            { new List<int> {27, 43, 79, 15, 85}, 72f },
+            { new List<int> {79, 81}, 49f },
+            { new List<int> {86, 56, 82, 96, 20, 43}, 113f },
+            { new List<int> {15, 70, 42, 71, 16}, 60f },
+            { new List<int> {8, 56, 53, 34, 75}, 63f },
+            { new List<int> {57, 98, 57}, 64f },
+            { new List<int> {46, 44, 61, 59}, 60f },
+            { new List<int> {100, 52, 48, 22, 75, 99}, 119f },
+            { new List<int> {92, 12, 18}, 34f },
+            { new List<int> {29, 43, 99, 60, 70, 15, 95}, 121f }
         };
 
         private static Dictionary<List<int>, float> bouncyBallTestCases = new Dictionary<List<int>, float>()
@@ -150,7 +131,7 @@ namespace GeneticProgramming
             
         };
 
-        private static Dictionary<List<int>, int> fibonacciNumbers = new Dictionary<List<int>, int>()
+        private static Dictionary<List<int>, float> fibonacciNumbers = new Dictionary<List<int>, float>()
         {
             { new List<int> { 0 }, 0 },
             { new List<int> { 1 }, 1 },
